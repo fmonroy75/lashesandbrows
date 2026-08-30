b3c38e06c1e69e60a2a8bf9786db2034d31a637a
fmonroy75 <francisco.monroy@gmail.com>
2026-08-30 00:15:37 -0400

    web

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,123 +1,131 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
 
-  devtools: { enabled: false },
+  devtools: {
+    enabled: false
+  },
 
   css: ['~/assets/css/main.css'],
 
   app: {
-  head: {
-    htmlAttrs: {
-      lang: 'en'
-    },
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
 
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png'
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      }
-    ],
-
-    meta: [
-      {
-        name: 'theme-color',
-        content: '#ffffff'
+    head: {
+      htmlAttrs: {
+        lang: 'en'
       },
 
-      {
-        name: 'description',
-        content:
-          'Luxury lash extensions, lash lift, brow lamination and professional brow design in Montréal.'
-      },
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'favicon.ico'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: 'favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: 'favicon-16x16.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: 'apple-touch-icon.png'
+        }
+      ],
 
-      {
-        property: 'og:site_name',
-        content: 'Lashes & Brows Montréal'
-      },
+      meta: [
+        {
+          name: 'theme-color',
+          content: '#ffffff'
+        },
 
-      {
-        property: 'og:type',
-        content: 'website'
-      },
+        {
+          name: 'description',
+          content:
+            'Luxury lash extensions, lash lift, brow lamination and professional brow design in Montréal.'
+        },
 
-      {
-        property: 'og:title',
-        content: 'Lashes & Brows Montréal'
-      },
+        {
+          property: 'og:site_name',
+          content: 'Lashes & Brows Montréal'
+        },
 
-      {
-        property: 'og:description',
-        content:
-          'Personalized lash and brow artistry in Montréal.'
-      },
+        {
+          property: 'og:type',
+          content: 'website'
+        },
 
-      {
-        property: 'og:url',
-        content: 'https://www.lashesandbrows.ca/'
-      },
+        {
+          property: 'og:title',
+          content: 'Lashes & Brows Montréal'
+        },
 
-      {
-        property: 'og:image',
-        content:
-          'https://www.lashesandbrows.ca/og-image.jpg'
-      },
+        {
+          property: 'og:description',
+          content:
+            'Personalized lash and brow artistry in Montréal.'
+        },
 
-      {
-        property: 'og:image:width',
-        content: '1200'
-      },
+        {
+          property: 'og:url',
+          content: 'https://www.lashesandbrows.ca/'
+        },
 
-      {
-        property: 'og:image:height',
-        content: '630'
-      },
+        {
+          property: 'og:image',
+          content: 'https://www.lashesandbrows.ca/og-image.jpg'
+        },
 
-      {
-        name: 'twitter:card',
-        content: 'summary_large_image'
-      },
+        {
+          property: 'og:image:width',
+          content: '1200'
+        },
 
-      {
-        name: 'twitter:title',
-        content: 'Lashes & Brows Montréal'
-      },
+        {
+          property: 'og:image:height',
+          content: '630'
+        },
 
-      {
-        name: 'twitter:description',
-        content:
-          'Personalized lash and brow artistry in Montréal.'
-      },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        },
 
-      {
-        name: 'twitter:image',
-        content:
-          'https://www.lashesandbrows.ca/og-image.jpg'
-      }
-    ]
-  }
-},
+        {
+          name: 'twitter:title',
+          content: 'Lashes & Brows Montréal'
+        },
+
+        {
+          name: 'twitter:description',
+          content:
+            'Personalized lash and brow artistry in Montréal.'
+        },
+
+        {
+          name: 'twitter:image',
+          content:
+            'https://www.lashesandbrows.ca/og-image.jpg'
+        }
+      ]
+    }
+  },
 
   routeRules: {
-    '/': { prerender: true },
-    '/es': { prerender: true }
+    '/': {
+      prerender: true
+    },
+
+    '/es': {
+      prerender: true
+    }
   },
 
   nitro: {
